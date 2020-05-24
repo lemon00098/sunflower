129e436d17f4ad49297ac40cc7ea9e5e38aa53d8
lemon00098 <62859259+lemon00098@users.noreply.github.com>
2020-05-24 22:15:26 +0900

    module
    practice

---
+++ path/to/Candystore.js
@@ -0,0 +1,7 @@
+import {Supermarket} from './Supermarket.js';
+
+export class Candystore extends Supermarket{
+  getCandystoreName(){
+    console.log('お菓子屋さんの名前はチロルです。');
+  }
+}
---
+++ path/to/Supermarket.js
@@ -0,0 +1,11 @@
+export class Supermarket {
+  constructor(){
+    this.price = 120;
+  }
+  getSupermarketName(){
+    console.log('Supermarketの名前はひまわりです');
+  }
+  getprice(){
+    return this.price;
+  }
+}
